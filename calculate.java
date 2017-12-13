729eb0c7796a536e316e77b97a3280c6b5172d54
MaximSofronov <maxim.sofronov@gmail.com>
2017-12-13 12:16:57 +0300

    mov 4, task /n calculate

--- calculate.java
+++ calculate.java
@@ -1,6 +1,23 @@
+
+
 public class calculate {
 	
 	public static void main(String[] arg) {
 		System.out.println("calculate");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		
+		double sum = first + second;
+		double sub = first - second;
+		double mul = first * second;
+		double div = first / second;
+
+		double deg = Math.pow(first, second);
+
+		System.out.println("sum = " + sum);
+		System.out.println("sub = " + sub);
+		System.out.println("mul = " + mul);
+		System.out.println("div = " + div);
+		System.out.println("deg = " + deg);
 	}
 }
